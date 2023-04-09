11c6a3bac86a9f3632277ada315670e79528f674
TiemiMorishita <105657256+TiemiMorishita@users.noreply.github.com>
2023-04-09 20:57:41 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,11 +1,11 @@
 function cor() {
 
-    const cores1 = '0123456789'
-    const cores2 = 'ABCDEF'
+    const cores1 = '0123456789';
+    const cores2 = 'ABCDEF';
 
     const cores  = cores1.concat(cores2); //concat
 
-    let texto = '#'
+    let texto = '#';
 
     for(let i = 0; i < 6; i++){ 
         texto += cores.charAt(Math.floor(Math.random() * cores.length)) //length e for
@@ -17,20 +17,20 @@ function cor() {
 
 function funcao1(){
 
-    let container = document.getElementById(`cor`)
-    container.style.backgroundColor = cor()
+    let container = document.getElementById(`cor`);
+    container.style.backgroundColor = cor();
         
-    let texto_cores = document.getElementById(`texto_cores`)
-    texto_cores.textContent = `${cor()}`
+    let texto_cores = document.getElementById(`texto_cores`);
+    texto_cores.textContent = `${cor()}`;
    
 }
 
 function funcao2(){
-    let borda = document.getElementById(`cor`)
+    let borda = document.getElementById(`cor`);
     borda.style.border = '#7e63a3 solid 3px';
 }
 
 function funcao3(){
-    let borda = document.getElementById(`cor`)
+    let borda = document.getElementById(`cor`);
     borda.style.border = 'none';
 }
